3a80ad2f43ddbb7ff10ff6ca42aa1c1d1024155f
tuntuntuntuntun <tuntuntun02b@gmail.com>
2020-10-05 19:06:44 +0900

    ログイン機能作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    logger('welcom route.');
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
